7d84a6289169363d31e08aedf8255330c998fc79
241083 <241083@std.hi-joho.ac.jp>
2025-05-16 13:08:17 +0900

    復習問題しました。

---
+++ src/Review1.java
@@ -0,0 +1,9 @@
+public class Review1 {
+    public static void main(String[] args) {
+        System.out.println("今日の天気は\t晴れです。");
+        System.out.println("今日は一日");
+        System.out.print("洗濯日和です。");
+        System.out.println("外に干しても大丈夫そうです。");
+        System.out.println("文字は'、文字列は”で囲みましょう。");
+    }
+}
