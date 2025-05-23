99013be2cf4bcb0278793bd0647f867e91b87829
241083 <241083@std.hi-joho.ac.jp>
2025-05-23 12:53:39 +0900

    復習問題28~29をしました。

---
+++ src/practice/reviews/reviews3/Review28_Email.java
@@ -0,0 +1,15 @@
+package practice.reviews.reviews3;
+
+public class Review28_Email {
+    public void send(String to, String body) {
+        System.out.println(to + " に以下のメールを送信しました。");
+        System.out.println("件名：【なし】");
+        System.out.println("本文：" + body);
+    }
+
+    public void send(String to, String subject, String body) {
+        System.out.println(to + " に以下のメールを送信しました。");
+        System.out.println("件名：" + subject);
+        System.out.println("本文：" + body);
+    }
+}
