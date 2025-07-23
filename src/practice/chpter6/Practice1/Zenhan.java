7771987362b67a3fe6b60c2f040edc7b5999ef48
241083 <241083@std.hi-joho.ac.jp>
2025-07-23 10:42:48 +0900

    加点課題

---
+++ src/practice/chpter6/Practice1/Zenhan.java
@@ -0,0 +1,10 @@
+package practice.chpter6.Practice1;
+
+public class Zenhan {
+    public static void doWarusa(){
+        System.out.println("きなこでござる。食えませんがの。");
+    }
+    public static void doTogame(){
+        System.out.println("このおいぼれの目はごまかせませんぞ。");
+    }
+}
