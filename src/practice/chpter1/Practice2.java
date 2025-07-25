98495cb8e33cf81621d771def897b4c111ca8534
241083 <241083@std.hi-joho.ac.jp>
2025-07-25 11:16:48 +0900

    復習問題をしました。

--- src/practice/chpter1/Practice2.java
+++ src/practice/chpter1/Practice2.java
@@ -6,6 +6,6 @@ public class Practice2 {
         int a = 3;
         int b = 5;
         int c = a * b;
-        System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + 15);
+        System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
     }
 }
